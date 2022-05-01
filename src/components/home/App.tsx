578c79c745a9a5d261362985368836ae9cc80b96
mahdy <a.b.mahdey@gmail.com>
2022-05-01 10:14:05 +0200

    intial commit- basic hierarchy-

---
+++ src/components/home/App.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from "react";
+// import logo from "../../assets/images/logo.svg";
+import "../../assets/css/home/App.css";
+
+const App: React.FC = (): ReactElement => {
+  return (
+    <div className="App">
+      <h1>Hello Germany</h1>
+    </div>
+  );
+};
+
+export default App;
